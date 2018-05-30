c4854684b747d77f72bac3f2322d97c1c5368e9d
beaverul <38052067+beaverul@users.noreply.github.com>
2018-05-30 20:00:52 +0300

    Add files via upload

---
+++ Temperature/Temperature.ino
@@ -0,0 +1,50 @@
+#include <dht.h>
+#include <Firebase.h>
+#include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+#define DHT11_PIN 13
+
+#include  <ESP8266WiFi.h>
+
+dht DHT;
+
+
+#define FIREBASE_HOST "smartalarmclock-5ad4f.firebaseio.com"
+#define WIFI_SSID "Smart" // Change the name of your WIFI
+#define WIFI_PASSWORD "puiuchu96" // Change the password of your WIFI
+
+
+void setup() {
+  
+  Serial.begin(9600);
+   WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  Firebase.begin(FIREBASE_HOST);
+}
+
+void tempSens()
+{
+  DHT.read11(DHT11_PIN);
+  float temperature = DHT.temperature;
+  delay(200);
+  Firebase.setFloat("Temp",temperature);
+  while(WiFi.status() != WL_CONNECTED){
+    WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+    delay(500);
+  }
+  delay(3000);
+ 
+}
+
+void loop() {
+         
+     tempSens();   
+
+}
+
